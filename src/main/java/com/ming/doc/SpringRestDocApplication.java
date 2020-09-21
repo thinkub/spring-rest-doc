b0f9fa00d20c66f127a6ff601f60169371837cce
이민호/TL개발팀 <thinkub0219@gmail.com>
2020-09-21 23:01:51 +0900

    init

---
+++ src/main/java/com/ming/doc/SpringRestDocApplication.java
@@ -0,0 +1,13 @@
+package com.ming.doc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestDocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ming/doc/SpringRestDocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ming.doc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestDocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
